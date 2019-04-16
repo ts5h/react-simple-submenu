26491f80da695e79ba773f504da4868732ededb0
at4 <at4@jc4.so-net.ne.jp>
2019-04-16 16:45:37 +0200

    Basic structure.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+
+import './scss/App.scss';
+
+// Contetns
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.<br/>
-              日本語テスト
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className={`box_header`}>Web Site Name</div>
+        
+        
+        {/*<header className="App-header">*/}
+        {/*  <img src={logo} className="App-logo" alt="logo" />*/}
+        {/*  <p>*/}
+        {/*    Edit <code>src/App.js</code> and save to reload.<br/>*/}
+        {/*      日本語テスト*/}
+        {/*  </p>*/}
+        {/*  <a*/}
+        {/*    className="App-link"*/}
+        {/*    href="https://reactjs.org"*/}
+        {/*    target="_blank"*/}
+        {/*    rel="noopener noreferrer"*/}
+        {/*  >*/}
+        {/*    Learn React*/}
+        {/*  </a>*/}
+        {/*</header>*/}
+  
+        {/* Contetns */}
+        <Main></Main>
+        <Footer></Footer>
       </div>
     );
   }
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class Footer extends React.Component {
+  render() {
+    return(
+      <div className={`box_footer`}>
+        <footer>
+          <p className={`copyright`}>&copy; Copyright</p>
+        </footer>
+      </div>
+    );
+  }
+}
