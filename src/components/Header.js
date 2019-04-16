c31b952d61fda7efd877e87f42db91cddfa77bc4
at4 <at4@jc4.so-net.ne.jp>
2019-04-16 21:34:58 +0200

    Completed basic mouse action

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,16 @@
 import React from "react";
 import Styles from '../scss/module/Header.module.scss';
 
+// Component
+import Submenu from './Submenu';
+
 export default class Header extends React.Component {
   render() {
     return(
       <div className={Styles.box_header}>
         <header className={`clearfix`}>
           <h1>Web Site Name</h1>
-          <ul className={Styles.main}>
-            <li>Menu 1
-              <ul className={Styles.sub}>
-                <li>Submenu 1</li>
-                <li>Submenu 2</li>
-                <li>Submenu 3</li>
-              </ul>
-            </li>
-            <li>Menu 2
-              <ul className={Styles.sub}>
-                <li>Submenu 4</li>
-                <li>Submenu 5</li>
-                <li>Submenu 6</li>
-                <li>Submenu 7</li>
-                <li>Submenu 8</li>
-              </ul>
-            </li>
-            <li>Menu 3</li>
-            <li>Menu 4</li>
-          </ul>
+          <Submenu></Submenu>
         </header>
       </div>
     );
