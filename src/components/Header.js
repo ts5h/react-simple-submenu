10fe5d1d72ea07d875781569ef62848bd5d88978
at4 <at4@jc4.so-net.ne.jp>
2019-04-16 21:56:38 +0200

    Feature: Formatted codes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,17 @@
 import React from "react";
-import Styles from '../scss/module/Header.module.scss';
+import Styles from "../scss/module/Header.module.scss";
 
 // Component
-import Submenu from './Submenu';
+import Submenu from "./Submenu";
 
+// Header
 export default class Header extends React.Component {
   render() {
-    return(
+    return (
       <div className={Styles.box_header}>
         <header className={`clearfix`}>
           <h1>Web Site Name</h1>
-          <Submenu></Submenu>
+          <Submenu />
         </header>
       </div>
     );
--- src/components/Submenu.js
+++ src/components/Submenu.js
@@ -1,31 +1,33 @@
 import React from "react";
-import Styles from '../scss/module/Header.module.scss';
+import Styles from "../scss/module/Header.module.scss";
 
 // Submenu
-export default class Header extends React.Component {
-  
+export default class Submenu extends React.Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
       menu1: false,
-      menu2: false,
-    }
+      menu2: false
+    };
   }
-  
-  submenu = (flag) => {
+
+  submenu = flag => {
     this.setState(flag);
-    //console.log(flag);
+    // console.log(flag);
   };
-  
+
   render() {
-    return(
+    return (
       <ul className={Styles.main}>
         <li
           onMouseOver={() => this.submenu({ menu1: true })}
           onMouseOut={() => this.submenu({ menu1: false })}
-        >Menu 1
-          <ul className={`${Styles.sub} ${this.state.menu1 ? Styles.show : ''}`}>
+        >
+          Menu 1
+          <ul
+            className={`${Styles.sub} ${this.state.menu1 ? Styles.show : ""}`}
+          >
             <li>Submenu 1</li>
             <li>Submenu 2</li>
             <li>Submenu 3</li>
@@ -34,8 +36,11 @@ export default class Header extends React.Component {
         <li
           onMouseOver={() => this.submenu({ menu2: true })}
           onMouseOut={() => this.submenu({ menu2: false })}
-        >Menu 2
-          <ul className={`${Styles.sub} ${this.state.menu2 ? Styles.show : ''}`}>
+        >
+          Menu 2
+          <ul
+            className={`${Styles.sub} ${this.state.menu2 ? Styles.show : ""}`}
+          >
             <li>Submenu 4</li>
             <li>Submenu 5</li>
             <li>Submenu 6</li>
